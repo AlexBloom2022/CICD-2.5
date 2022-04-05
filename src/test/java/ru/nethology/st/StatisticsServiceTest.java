c5caa611cbffaa8347e18a6a467e6fa1648af61e
aleksandraurchenko <alex_bloom@mail.ru>
2022-04-05 15:00:38 +0300

    added StatisticsServiceTest2

--- src/test/java/ru/nethology/st/StatisticsServiceTest.java
+++ src/test/java/ru/nethology/st/StatisticsServiceTest.java
@@ -6,16 +6,16 @@ package ru.nethology.st;
 import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
-        @Test
-        void findMax() {
-            StatisticsService service = new StatisticsService();
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
 
-            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
-            long expected = 15;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-            long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-            assertEquals(expected, actual);
-        }
+        assertEquals(expected, actual);
 
     }
+}
